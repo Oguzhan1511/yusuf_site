16706b02bc4141ad9df46e284f471c4e03a09759
Oğuzhan Özdemir <oguzhanozdemir@Oguzhan-MacBook-Air.local>
2026-07-04 18:52:56 +0300

    feat: add Google Tag (gtag.js) to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -61,6 +61,19 @@ export default function RootLayout({
   return (
     <html lang="tr">
       <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-18295089478"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'AW-18295089478');
+            `,
+          }}
+        />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
